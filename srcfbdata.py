1515591a3b468ce5e90c7b43ec09ed706250ba90
Michael V. DePalatis <depalatis@gmail.com>
2014-12-20 20:32:38 +0100

    Winner/Loser -> Away/Home in columns
    Some things still to do:
    
    * Remove unplayed games from the schedule
    * Remove rank numbers from team names
    * Thoroughly check data integrity
    * Combine date and time into one datetime field

--- srcfbdata.py
+++ srcfbdata.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Scrape CFB data from sports-reference.com."""
 
 from __future__ import print_function
@@ -6,7 +7,7 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado import gen, ioloop
 import pandas as pd
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__file__.split('.')[0])
 
 def _schedule_url(year):
@@ -18,7 +19,7 @@ def download_schedule(year):
     """Get the full schedule from the specified year."""
     assert isinstance(year, int)
     url = _schedule_url(year)
-    logger.info('Getting data for {:d}'.format(year))
+    logger.info('Getting schedule data for {:d}...'.format(year))
     logger.debug('URL: {:s}'.format(url))
     response = yield AsyncHTTPClient().fetch(url)
     schedule = pd.read_html(response.body, attrs={'id': 'schedule'})[0]
@@ -26,8 +27,7 @@ def download_schedule(year):
     # Get rid of rows that are just repeating the header (which is
     # useful for viewing on an HTML page, but not useful as a table
     # for manipulation!).
-    bad_rows = schedule[schedule.Rk.str.match('Rk')]
-    schedule = schedule.drop(bad_rows)
+    schedule = schedule[~schedule.Rk.str.match('Rk')]
 
     # Rename columns. This is mostly cosmetic for purposes of making
     # it more obvious what we're doing to the table later.
@@ -37,20 +37,32 @@ def download_schedule(year):
             'Winner/Tie': 'Winner',
             'Pts': 'WinnerPoints',
             'Unnamed: 7': 'HomeOrAway',
-            'Lower/Tie': 'Loser',
+            'Loser/Tie': 'Loser',
             'Pts.1': 'LoserPoints',
             'Unnamed: 12': 'Empty'},
         inplace=True)
 
-    # TODO: mask '@' in HomeOrAway so that columsn can be reordered
-    # with away always first and home always second.
+    # Swap team columns so that away is always on the left and home is
+    # always on the right and rename the columns appropriately.
+    home_won = schedule.HomeOrAway.isnull()
+    schedule.Winner[home_won], schedule.Loser[home_won] = schedule.Loser[home_won], schedule.Winner[home_won]
+    schedule.WinnerPoints[home_won], schedule.LoserPoints[home_won] = schedule.LoserPoints[home_won], schedule.WinnerPoints[home_won]
+
+    # Rename Winner/Loser to Away/Home
+    schedule.rename(
+        columns={
+            'Winner': 'Away',
+            'Loser': 'Home',
+            'WinnerPoints': 'AwayPoints',
+            'LoserPoints': 'HomePoints'},
+        inplace=True)
 
     # TODO: remove unplayed games from the schedule
 
     # Remove unnecessary columns.
-    schedule.drop('Week')
-    schedule.drop('Day', 1)
-    schedule.drop('Empty')
+    schedule.drop(
+        ['Rk', 'Week', 'Day', 'HomeOrAway', 'Empty', 'TV', 'Notes'],
+        axis=1, inplace=True)
     
     raise gen.Return(schedule)
 
@@ -58,10 +70,10 @@ def download_schedule(year):
 def main():
     """Main function."""
     schedule = yield download_schedule(2014)
-    schedule.to_csv('schedule_2014.csv')
+    schedule.to_csv('schedule_2014.csv', encoding='utf-8', index=False)
 
 if __name__ == "__main__":
-    if False:
+    if True:
         ioloop.IOLoop.instance().run_sync(main)
     else:
         schedule = pd.read_html(_schedule_url(2014), attrs={'id': 'schedule'})[0]
